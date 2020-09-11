f667ef2da39924b6b769b4fc926ad27dc369108d
王超(外包) <wbwangchao1@dfzq.dfzq.ad>
2020-09-11 15:51:56 +0800

    breadcrumb

---
+++ examples/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h2>button</h2>
+    <test-button></test-button>
+    <h2>breadcrumb</h2>
+    <test-breadcrumb></test-breadcrumb>
+  </div>
+</template>
+
+<script>
+import TestButton from './TestButton'
+import TestBreadcrumb from './TestBreadcrumb'
+// console.log(TestButton)
+export default {
+  components: {
+    TestButton,
+    TestBreadcrumb
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.row {
+  padding: 10px 0;
+}
+.hm-button {
+  margin-left: 10px;
+}
+</style>
